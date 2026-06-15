9df00738ecd905ed8ebe1cac5f0f07776b889db1
amanxridex <you@example.com>
2026-06-15 23:21:18 +0530

    Update gallery heading text

---
+++ update_gallery_text.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+
+const targetDir = 'c:/Users/91836/PERSONAL/edinmaster/ED';
+const files = fs.readdirSync(targetDir).filter(f => f.endsWith('.html'));
+
+files.forEach(file => {
+    const filePath = path.join(targetDir, file);
+    let html = fs.readFileSync(filePath, 'utf8');
+    
+    // Case insensitive global replacement
+    const regex = /Displaying Your Creations/gi;
+    if (regex.test(html)) {
+        html = html.replace(regex, 'Displaying Our Top Projects');
+        fs.writeFileSync(filePath, html, 'utf8');
+        console.log('Updated Gallery text in ' + file);
+    }
+});
